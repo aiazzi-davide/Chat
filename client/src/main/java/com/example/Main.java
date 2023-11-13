f68c42e5a8478bfe89db24f631c0677e08e4ceb5
lai2-24 <lai2-24@itismeucci.com>
2023-11-13 10:11:14 +0100

    gitignore

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hsohgiuohgiofdhfio");
-        System.out.println("sdyhudsgugdnfdsbfn");
+        
     }
 }
