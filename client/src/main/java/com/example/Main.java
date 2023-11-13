ee61f0932c9f067c4655ba976960f9cf3b74a063
lai2-24 <lai2-24@itismeucci.com>
2023-11-13 09:58:35 +0100

    START

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -2,9 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("PROVA");
-        System.out.println("ESTOCCAS");
-        System.out.println("ESDRAGONGOGANGORANGOESRAGONGASIMO");
+        
     }
 }
--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -2,9 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("prova gang");
-        System.out.println("prova gang");
-        System.out.println("prova gang");
+        
 
     }
 }
