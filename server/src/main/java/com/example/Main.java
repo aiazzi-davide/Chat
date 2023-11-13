af44cd7d4fff8c906066c07fb3fea6b9e1d6e150
lai2-24 <lai2-24@itismeucci.com>
2023-11-13 09:56:48 +0100

    prova3456

--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -3,5 +3,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("prova gang");
+        System.out.println("prova gang");
+        System.out.println("prova gang");
+
     }
 }
