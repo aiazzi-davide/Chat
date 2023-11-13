d30bcb2bf525447722c9be245eb614248011db0f
lai2-24 <lai2-24@itismeucci.com>
2023-11-13 09:56:13 +0100

    WEFDEFEF

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("PROVA");
         System.out.println("ESTOCCAS");
+        System.out.println("ESDRAGONGOGANGORANGOESRAGONGASIMO");
     }
 }
